da1321a92dec629ffc75ba24b8f1439bcf2e1c27
Matthew <kfm.yong@gmail.com>
2020-06-30 19:50:34 +0000

    Add template code from emailjs.com to contact.js

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,15 +1,15 @@
-//Function to send message from contact form to emailaddress. Code used from CI course 'sending an email with emailjs'
-$("#send-button").on("click", function() {
-    $("#send-button").submit(function() {
-        var template_params = {
-            "first_name": "last_name_value",
-            "last_name": "last_name_value",
-            "email_address": "email_address_value",
-            "message": "message_value"
+//Function to send message from contact form to emailaddress. Code used from emailjs.com "template-code-sample"
+function sendEmail(Form) {
+    var template_params = {
+            "first_name": Form.fname.value,
+            "last_name": Form.lname.value,
+            "email_address": Form.email.value,
+            "message": Form.message.value,  
         };
-        var service_id = "default_service";
-        var template_id = "twufo";
-        emailjs.send(service_id, template_id, template_params);
-        console.log("test");
-    });
-});
+
+    var service_id = "gmail";
+    var template_id = "twufo";
+    emailjs.send(service_id, template_id, template_params);
+    return false;
+};
+
