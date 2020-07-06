f1c7212f09f8f10212c53f5899706274af5d0506
Matthew <kfm.yong@gmail.com>
2020-07-06 21:30:19 +0000

    Update jasmine testing

--- assets/game-testing/spec/twufo-jasmine-Spec.js
+++ assets/game-testing/spec/twufo-jasmine-Spec.js
@@ -1,6 +1,39 @@
 //Testing the volume button
-describe("When volume icon is visible", function() {
-    it("Should have show volume icon and have sound", function() {
+describe("When volume icon is clicked", function() {
+    it("should turn off sound, hide the icon and how mute icon", function() {
+        expect($("#volume")).toHaveClass("hide-button");
+        expect($("#mute")).not.toHaveClass("hide-button");
+        expect($("audio")).toHaveProp("muted");
+    });
+
+
+/*
+    it("Should show volume icon", function() {
         expect($("#volume")).not.toHaveClass("hide-button");
     });
+    it("Should hide the mute button", function() {
+        expect($("#mute")).toHaveClass("hide-button");
+    });
+    it("Should have sound", function() {
+        expect($("audio")).not.toHaveProp("muted");
+    });  */
 });
+
+
+
+
+  /*  var spyEvent = spyOnEvent('#volume', 'click')
+    $('#volume').click()
+
+    it("Should hide the volume icon", function() {
+        expect(spyEvent).toHaveBeenTriggered('.hide-button')
+    });*/
+
+
+  /*  //Testing the volume button
+describe("When start button is clicked", function() {
+    it("Should hide the button", function() {
+        newGame();
+        expect($("#start")).toHaveClass("hide-button");
+    });
+});*/
