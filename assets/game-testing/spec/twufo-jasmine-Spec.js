17e3f0a47c416853ef72b1aee3147e254af9d8db
Matthew <kfm.yong@gmail.com>
2020-07-08 20:45:03 +0000

    Fix bug setFixtures by using backtick

--- assets/game-testing/spec/twufo-jasmine-Spec.js
+++ assets/game-testing/spec/twufo-jasmine-Spec.js
@@ -1,14 +1,14 @@
 //Testing the volume button
 describe("When volume button is clicked", function() {
     it("should hide volume, show mute button and mute sound", function() {
-        setFixtures(
-            '<div id="volume"></div>',
-            '<div id="mute"></div>',
-            '<div></div>');
+        setFixtures(`
+            <div id="volume"></div>
+            <div id="mute"></div>);            
+            <audio></audio>`);
         volumeOff();
         expect($("#volume")).toHaveClass("hide-button");
         expect($("#mute")).not.toHaveClass("hide-button");
-        expect($("audio")).toHaveProp("muted");
+        expect($("audio")).toHaveProp("muted", true);
     });
 });
 
