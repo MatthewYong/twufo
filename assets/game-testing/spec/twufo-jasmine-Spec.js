937eaec5311cc059ad3d7cd6a10c2f9b2b1ca949
Matthew <kfm.yong@gmail.com>
2020-07-14 19:51:21 +0000

    Beautified code on jasmine.html and spec.js

--- assets/game-testing/spec/twufo-jasmine-Spec.js
+++ assets/game-testing/spec/twufo-jasmine-Spec.js
@@ -1,13 +1,13 @@
 //Testing the volume button
-describe("When volume button is clicked", function() {
-    beforeEach(function() {
+describe("When volume button is clicked", function () {
+    beforeEach(function () {
         setFixtures(`
             <div id="volume"></div>
             <div id="mute"></div>);            
             <audio></audio>`);
     });
-    
-    it("should hide volume, show mute button and mute sound", function() {
+
+    it("should hide volume, show mute button and mute sound", function () {
         volumeOff();
         expect($("#volume")).toHaveClass("hide-button");
         expect($("#mute")).not.toHaveClass("hide-button");
@@ -16,15 +16,15 @@ describe("When volume button is clicked", function() {
 });
 
 //Testing the mute button
-describe("When mute button is clicked", function() {
-    beforeEach(function() {
+describe("When mute button is clicked", function () {
+    beforeEach(function () {
         setFixtures(`
             <div id="volume"></div>
             <div id="mute"></div>);            
             <audio></audio>`);
-    });    
+    });
 
-    it("should hide mute, show volume button and add sound", function() {
+    it("should hide mute, show volume button and add sound", function () {
         volumeOn();
         expect($("#volume")).not.toHaveClass("hide-button");
         expect($("#mute")).toHaveClass("hide-button");
@@ -33,13 +33,13 @@ describe("When mute button is clicked", function() {
 });
 
 //Testing the start button
-describe("When start button is clicked", function() {
-    beforeEach(function() {
+describe("When start button is clicked", function () {
+    beforeEach(function () {
         setFixtures(`
             <div id="start"></div>`);
-    });    
+    });
 
-    it("should hide start button and reset all values", function() {
+    it("should hide start button and reset all values", function () {
         newGame();
         expect($("#start")).toHaveClass("hide-button");
         expect(game.computerSequence.length).toEqual(0);
@@ -50,35 +50,35 @@ describe("When start button is clicked", function() {
 });
 
 //Testing the continue button
-describe("When continue button is clicked", function() {
-    beforeEach(function() {
+describe("When continue button is clicked", function () {
+    beforeEach(function () {
         setFixtures(`
             <div id="continue"></div>`);
     });
 
-    it("should hide continue button and reset values", function() {
+    it("should hide continue button and reset values", function () {
         cont();
         expect($('#continue')).toHaveClass("hide-button");
         expect($('#continue')).not.toHaveAttr("click");
         expect(game.playerSequence.length).toEqual(0);
-        expect(game.runningSequence.length).toEqual(0);               
+        expect(game.runningSequence.length).toEqual(0);
     });
 });
 
 //Testing the retry button
-describe("When retry button is clicked", function() {
-    beforeEach(function() {
+describe("When retry button is clicked", function () {
+    beforeEach(function () {
         setFixtures(`
             <div id="retry"></div>);
             <div id="start"></div>);
-            <div id="level"></div>`);            
+            <div id="level"></div>`);
     });
 
-    it("should hide continue button and reset values", function() {
+    it("should hide continue button and reset values", function () {
         retry();
         expect($('#retry')).toHaveClass("hide-button");
-        expect($('#start')).not.toHaveClass("hide-button");  
-        expect($('#level')).toContainText("Level 0");                     
+        expect($('#start')).not.toHaveClass("hide-button");
+        expect($('#level')).toContainText("Level 0");
     });
 });
 
